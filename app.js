b766542a8ceeb70ebf4112f3145d5febcd7f49a9
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-03-01 16:59:32 +0800

    Add POST edit page

--- app.js
+++ app.js
@@ -91,12 +91,37 @@ app.post('/restaurants', (req, res) => {
 
 // 修改 restaurant 頁面
 app.get('/restaurants/:id/edit', (req, res) => {
-  res.send('修改 restaurant 頁面')
+  Restaurant.findById(req.params.id)
+    .lean()
+    .exec((err, restaurant) => {
+      if (err) return console.error(err)
+      return res.render('edit', { restaurant: restaurant })
+    })
 })
+
 // 修改 restauarnt
 app.post('/restaurants/:id/edit', (req, res) => {
-  res.send('修改 restaurant')
+  Restaurant.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err)
+    restaurant.name = req.body.name
+    restaurant.name_en = req.body.name_en
+    restaurant.category = req.body.category
+    restaurant.image = req.body.image
+    restaurant.location = req.body.location
+    restaurant.phone = req.body.phone
+    restaurant.google_map = req.body.google_map
+    restaurant.rating = req.body.rating
+    restaurant.description = req.body.description
+
+    restaurant.save(err => {
+      if (err) return console.error(err)
+      return res.redirect(`/restaurants/${req.params.id}`)
+    })
+  })
 })
+
+
+
 // 刪除 restaurant
 app.post('/restaurants/:id/delete', (req, res) => {
   res.send('刪除 restaurant')
