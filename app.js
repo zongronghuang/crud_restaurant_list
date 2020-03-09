e694e04fd8034877880421f3bf45491ed20a10ab
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-03-10 01:11:30 +0800

    Modify redirect path

--- app.js
+++ app.js
@@ -62,6 +62,7 @@ app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.success_msg = req.flash('success_msg')
   res.locals.warning_msg = req.flash('warning_msg')
+
   next()
 })
 
--- routes/user.js
+++ routes/user.js
@@ -13,7 +13,7 @@ router.get('/login', (req, res) => {
 router.post('/login', (req, res, next) => {
   passport.authenticate('local', {
     successRedirect: '/',
-    failureRedirect: 'users/login'
+    failureRedirect: '/users/login'
   })(req, res, next)
 })
 
