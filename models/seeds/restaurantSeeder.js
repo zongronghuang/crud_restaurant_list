1420a47ab803a507d06faa5debdbb34fa5872333
HUANG Zong-Rong <maibells0@yahoo.com.tw>
2020-03-11 17:51:17 +0800

    Seed restaurants with user information

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Restaurant = require('../restaurant.js')
-const User = require('../models/user.js')
+const User = require('../user.js')
 const restaurantList = require('./restaurant.json')
 const results = restaurantList.results
 
@@ -15,19 +15,64 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('db connected')
 
-  for (let i = 0; i < results.length; i++) {
-    Restaurant.create({
-      name: results[i].name,
-      name_en: results[i].name_en,
-      category: results[i].category,
-      image: results[i].image,
-      location: results[i].location,
-      phone: results[i].phone,
-      google_map: results[i].google_map,
-      rating: results[i].rating,
-      description: results[i].description,
+  User.find()
+    .lean()
+    .exec((err, users) => {
+      if (err) return console.error(err)
+
+      const restaurants = [...results]
+
+      for (let j = 0; j < 3; j++) {
+        Restaurant.create({
+          name: restaurants[j].name,
+          name_en: restaurants[j].name_en,
+          category: restaurants[j].category,
+          image: restaurants[j].image,
+          location: restaurants[j].location,
+          phone: restaurants[j].phone,
+          google_map: restaurants[j].google_map,
+          rating: restaurants[j].rating,
+          description: restaurants[j].description,
+          userId: users[1]._id
+        })
+      }
+
+      for (let j = 3; j < 6; j++) {
+        Restaurant.create({
+          name: restaurants[j].name,
+          name_en: restaurants[j].name_en,
+          category: restaurants[j].category,
+          image: restaurants[j].image,
+          location: restaurants[j].location,
+          phone: restaurants[j].phone,
+          google_map: restaurants[j].google_map,
+          rating: restaurants[j].rating,
+          description: restaurants[j].description,
+          userId: users[1]._id
+        })
+      }
+
     })
-  }
+
+
+
+  // })
+
+
+
+  // for (let i = 0; i < results.length; i++) {
+  //   Restaurant.create({
+  //     name: results[i].name,
+  //     name_en: results[i].name_en,
+  //     category: results[i].category,
+  //     image: results[i].image,
+  //     location: results[i].location,
+  //     phone: results[i].phone,
+  //     google_map: results[i].google_map,
+  //     rating: results[i].rating,
+  //     description: results[i].description,
+  //   })
+  // }
 
   console.log('done')
 })
