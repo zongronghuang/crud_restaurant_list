f724da73ab378f848d08c32d401adbddbbc3a695
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-06-08 01:09:35 +0800

    refactor: modify seeder file

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,51 +1,148 @@
+const bcrypt = require('bcryptjs')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const db = require('../../config/mongoose.js')
 const Restaurant = require('../restaurant.js')
 const User = require('../user.js')
-const restaurantList = require('./restaurant.json')
-const results = restaurantList.results
+const results1 = require('./restaurant1.json').results
+const results2 = require('./restaurant2.json').results
+
+const SEED_USER1 = {
+  name: 'user1',
+  email: 'user1@example.com',
+  password: '12345678'
+}
+
+const SEED_USER2 = {
+  name: 'user2',
+  email: 'user2@example.com',
+  password: '12345678'
+}
 
 db.once('open', () => {
-  console.log('db connected')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER1.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER1.name,
+      email: SEED_USER1.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
 
-  User.find()
-    .lean()
-    .exec((err, users) => {
-      if (err) return console.error(err)
+      return Promise.all(Array.from(
+        { length: 3 },
+        (_, i) => {
+          console.log('i', i)
+          Restaurant.create({
+            name: results1[i].name,
+            name_en: results1[i].name_en,
+            category: results1[i].category,
+            image: results1[i].image,
+            location: results1[i].location,
+            phone: results1[i].phone,
+            google_map: results1[i].google_map,
+            rating: results1[i].rating,
+            description: results1[i].description,
+            userId
+          })
+        }
 
-      const restaurants = [...results]
-
-      for (let j = 0; j < 3; j++) {
-        Restaurant.create({
-          name: restaurants[j].name,
-          name_en: restaurants[j].name_en,
-          category: restaurants[j].category,
-          image: restaurants[j].image,
-          location: restaurants[j].location,
-          phone: restaurants[j].phone,
-          google_map: restaurants[j].google_map,
-          rating: restaurants[j].rating,
-          description: restaurants[j].description,
-          userId: users[0]._id
-        })
-      }
-
-      for (let j = 3; j < 6; j++) {
-        Restaurant.create({
-          name: restaurants[j].name,
-          name_en: restaurants[j].name_en,
-          category: restaurants[j].category,
-          image: restaurants[j].image,
-          location: restaurants[j].location,
-          phone: restaurants[j].phone,
-          google_map: restaurants[j].google_map,
-          rating: restaurants[j].rating,
-          description: restaurants[j].description,
-          userId: users[1]._id
-        })
-      }
+      ))
 
     })
+    .then(() => {
+      console.log('User1 done')
+    })
+    .catch(error => console.log(error))
 
-  console.log('done')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER2.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER2.name,
+      email: SEED_USER2.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+
+      return Promise.all(Array.from(
+        { length: 3 },
+        (_, j) => {
+          console.log('j', j)
+
+          Restaurant.create({
+            name: results2[j].name,
+            name_en: results2[j].name_en,
+            category: results2[j].category,
+            image: results2[j].image,
+            location: results2[j].location,
+            phone: results2[j].phone,
+            google_map: results2[j].google_map,
+            rating: results2[j].rating,
+            description: results2[j].description,
+            userId
+          })
+
+        }
+
+      ))
+
+    })
+    .then(() => {
+      console.log('User 2 done')
+    })
+    .catch(error => console.log(error))
 })
+
+
+
+// db.once('open', () => {
+//   bcrypt
+//     .genSalt(10)
+//     .then(salt => bcrypt.hash(SEED_USER2.password, salt))
+//     .then(hash => User.create({
+//       name: SEED_USER2.name,
+//       email: SEED_USER2.email,
+//       password: hash
+//     }))
+//     .then(user => {
+//       const userId = user._id
+
+//       return Promise.all(Array.from(
+//         { length: 3 },
+//         (_, j) => {
+//           console.log('j', j)
+
+//           Restaurant.create({
+//             name: results2[j].name,
+//             name_en: results2[j].name_en,
+//             category: results2[j].category,
+//             image: results2[j].image,
+//             location: results2[j].location,
+//             phone: results2[j].phone,
+//             google_map: results2[j].google_map,
+//             rating: results2[j].rating,
+//             description: results2[j].description,
+//             userId
+//           })
+
+//         }
+
+//       ))
+
+//     })
+//     .then(() => {
+//       console.log('User 2 done')
+//       process.exit()
+//     })
+// })
+
+
+
 
